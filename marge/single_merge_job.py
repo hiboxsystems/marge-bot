4f4fb3f90bbf34208b6a5c62e201773377adb1d6
Sebastian Lövdahl <slovdahl@hibox.fi>
2025-02-03 17:21:47 +0200

    Sleep even more

--- marge/single_merge_job.py
+++ marge/single_merge_job.py
@@ -121,7 +121,7 @@ class SingleMergeJob(MergeJob):
 
             if target_project.only_allow_merge_if_pipeline_succeeds:
                 self.wait_for_ci_to_pass(merge_request, actual_sha)
-                time.sleep(4)
+                time.sleep(6)
 
             self.wait_for_merge_status_to_resolve(merge_request)
 
